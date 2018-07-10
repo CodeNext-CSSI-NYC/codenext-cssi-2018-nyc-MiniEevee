5a584d29f0d4d3a1867b79b0f8a2043adc0429f1
Julia Yu <rubyjoycat@gmail.com>
2018-07-10 11:13:24 -0400

    only instructions

--- easter-predictor.js
+++ easter-predictor.js
@@ -18,3 +18,14 @@ var p = (h + r - 7 * m + 114) % 31;
 
 console.log("Month: " + Math.round(n));
 console.log("Day: " + Math.round(p + 1));
+
+// Divide y by 19 and call the remainder a. Ignore the quotient.
+// Divide y by 100 and get a quotient b and remainder c.
+// Divide b by 4 and get quotient d and remainder e.
+// Divide b + 8 by 25 and get quotient f. Ignore the remainder.
+// Divide b - f + 1 by 3 and get quotient g. Ignore the remainder.
+// Divide 19 * a + b - d - g + 15 by 30 and get remainder h. Ignore the quotient.
+// Divide c by 4 and get quotient i and remainder k.
+// Divide 32 + 2 * e + 2 * i - h - k by 7 and get remainder r. Ignore the quotient.
+// Divide a + 11 * h + 22 * r by 451 and get quotient m. Ignore the remainder.
+// Divide h + r - 7 * m + 114 by 31 and get quotient n and remainder p.
