36891e26ec42336cc1bda70f1fc46fbebbc9a5dd
Julia Yu <rubyjoycat@gmail.com>
2018-07-10 11:13:13 -0400

    user imputting name

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
-console.log("Hello World");
-console.log("Hi.");
-console.log("It's a beautiful day for programming, isn't it?");
-console.log("Hey! What's 1 + 1?");
-console.log("It's " + 1 + 1 + " of course!");
-console.log("No, it's " + (1 + 1) + ", silly goose!");
+var readline = require("readline-sync");
+var userName = readline.question("Please enter your name: ");
+
+console.log("Hello " + userName + "!");
+// console.log("Hi.");
+// console.log("It's a beautiful day for programming, isn't it?");
+// console.log("Hey! What's 1 + 1?");
+// console.log("It's " + 1 + 1 + " of course!");
+// console.log("No, it's " + (1 + 1) + ", silly goose!");
 // console.log("This isn't going to get printed, is it?");
