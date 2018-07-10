ad285297b372a2cb5239a217218a208e35616f75
Julia Yu <rubyjoycat@gmail.com>
2018-07-10 12:17:22 -0400

    finished change machine

--- change-machine.js
+++ change-machine.js
@@ -2,24 +2,40 @@
 
 let readline = require("readline-sync");
 let money = parseInt(readline.question("Please enter an amount of money in cents: "));
-let quarters = parseInt(Math.floor(money/ 25));
-let dimes = parseInt(Math.floor(money - 25 * quarters/ 10));
-let nickels = parseInt(Math.floor(money - (25 * quarters + 10 * dimes)/ 5));
-let pennies = parseInt(Math.floor(money - (25 * quarters + 10 * dimes + 5 * nickels)/ 1));
-
-if(money - 25 * quarters <= 0){
-  dimes = 0;
-  nickels = 0;
-  pennies = 0;
-}
-else{
-  if(money - 25 * quarters >= 0){
-    
-  }
-}
-
 console.log(money + " cents makes: ");
+const quarters = Math.floor(money / 25);
 console.log(quarters + " quarter(s)");
+const dimes = Math.floor((money - (25 * quarters)) / 10);
 console.log(dimes + " dime(s)");
+const nickels = Math.floor((money - ((25 * quarters) + (10 * dimes))) / 5);
 console.log(nickels + " nickel(s)");
+const pennies = Math.floor((money - ((25 * quarters) + (10 * dimes) + (5 * nickels))) / 1);
 console.log(pennies + " penny(ies)");
+
+// if(money - 25 * quarters <= 0){
+//   dimes = 0;
+//   nickels = 0;
+//   pennies = 0;
+// }
+// else{
+//   if(money - 25 * quarters > 0){
+//     if(money - 10 * dimes <= 0){
+//       nickels = 0;
+//       pennies = 0;
+//     }
+//     else{
+//       if(money - 10 * dimes > 0){
+//         if(money - 5 * nickels <= 0){
+//           pennies = 0;
+//         }
+//         else{
+//           if(money - 5 * nickels > 0){
+//             if(money - 1 * pennies > 0){
+//               pennies = money;
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
