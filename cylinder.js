99070e4f830a4a91c920bc167927efd27cc311c7
Julia Yu <rubyjoycat@gmail.com>
2018-07-10 15:31:37 -0400

    cylinder calculator

--- cylinder.js
+++ cylinder.js
@@ -2,26 +2,8 @@ let readline = require("readline-sync");
 let radius = readline.question("What is the radius? ");
 let height = readline.question("What is the height? ");
 let volume = readline.question("Do you want to know the volume? ");
-let surfaceArea = roadline.question("Do you want to know the surface area? ");
-let x = 0;
-let y = 0;
+let surfaceArea = readline.question("Do you want to know the surface area? ");
 
-if(volume == yes){
-  x = ((Math.PI) * (radius ** 2) * height);
-  console.log(x);
-}
-else{
-  if(volume == no){
-    x = 0;
-  }
-}
+console.log("Volume of a cylinder with a radius of " + radius + " and a height of " + height + " is " + ((Math.PI) * (radius ** 2) * height));
 
-if(surfaceArea == yes){
-  y = (2 * Math.PI * radius * height) + (2 * Math.PI * (radius ** 2));
-  console.log(y);
-}
-else{
-  if(surfaceArea = no){
-    y = 0;
-  }
-}
+console.log("Surface area of a cylinder with a radius of " + radius + " and a height of " + height + " is " + (2 * Math.PI * radius * height) + (2 * Math.PI * (radius ** 2)));
