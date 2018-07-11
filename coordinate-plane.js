707968778cb03a5aac845f37ce89ab36ec311e24
Julia Yu <rubyjoycat@gmail.com>
2018-07-11 13:58:42 -0400

    ITS FINISHED

--- coordinate-plane.js
+++ coordinate-plane.js
@@ -1,20 +1,20 @@
 let readline = require("readline-sync");
-let x1 = readline.question("Enter X-Coordinate 1: ");
-let y1 = readline.question("Enter Y-Coordinate 1: ");
-let x2 = readline.question("Enter X-Coordinate 2: ");
-let y2 = readline.question("Enter Y-Coordinate 2: ");
-let length = readline.question("Do you want to the length? ");
-let midpoint = readline.question("Do you want to know the midpoint? ");
+let x1 = parseInt(readline.question("Enter X-Coordinate 1: "));
+let y1 = parseInt(readline.question("Enter Y-Coordinate 1: "));
+let x2 = parseInt(readline.question("Enter X-Coordinate 2: "));
+let y2 = parseInt(readline.question("Enter Y-Coordinate 2: "));
+let length = readline.question("Do you want to the length? ").toUpperCase();
+let midpoint = readline.question("Do you want to know the midpoint? ").toUpperCase();
 
-if(length.length  == 3){
+if(length == "YES"){
   console.log("The length is: " + Math.sqrt(((x2 - x1)**2) + ((y2 - y1) ** 2)));
 }
 else{
   console.log("Okay");
   }
 
-if(midpoint.length == 3){
-  console.log("The midpoint is: " + "(" + (x1 + x2) / 2 + ", " + (y1 + y2) / 2 + ")");
+if(midpoint == "YES"){
+  console.log("The midpoint is: " + "(" + ((x1 + x2) / 2) + ", " + ((y1 + y2) / 2) + ")");
 }
 else{
   console.log("Okay");
