896b48af44a8348b6d38f65446929df71288407e
Julia Yu <rubyjoycat@gmail.com>
2018-07-12 12:57:50 -0400

    my suffering

---
+++ which-day.js
@@ -0,0 +1,108 @@
+// Declare a function whichDay that takes two parameters:
+// A day of the week, and a number of days from now.
+// It should return the day of the week it will be that many days from now.
+
+function whichDay(day, time) {
+
+    if(time % 7 == 1) {
+      if(day == "Monday"){
+        return("Tuesday");
+      }
+      else if(day == "Tuesday") {
+        return("Wednesday");
+      }
+      else if(day == "Wednesday") {
+        return("Thursday");
+      }
+      else if(day == "Thursday") {
+        return("Friday");
+      }
+      else if(day == "Friday") {
+        return("Saturday");
+      }
+      else if(day == "Saturday") {
+        return("Sunday");
+      }
+      else if(day == "Sunday") {
+        return("Monday");
+      }
+    }
+  }
+
+      if(time % 7 == 2) {
+        if(day == "Monday"){
+          return("Wednesday");
+        }
+        else if(day == "Tuesday") {
+          return("Thursday");
+        }
+        else if(day == "Wednesday") {
+          return("Friday");
+        }
+        else if(day == "Thursday") {
+          return("Saturday");
+        }
+        else if(day == "Friday") {
+          return("Sunday");
+        }
+        else if(day == "Saturday") {
+          return("Monday");
+        }
+        else if(day == "Sunday") {
+          return("Tuesday");
+        }
+      }
+
+          if(time % 7 == 3) {
+            if(day == "Monday"){
+              return("Thursday");
+            }
+            else if(day == "Tuesday") {
+              return("Friday");
+            }
+            else if(day == "Wednesday") {
+              return("Saturday");
+            }
+            else if(day == "Thursday") {
+              return("Sunday");
+            }
+            else if(day == "Friday") {
+              return("Monday");
+            }
+            else if(day == "Saturday") {
+              return("Tuesday");
+            }
+            else if(day == "Sunday") {
+              return("Wednesday");
+            }
+          }
+          
+              if(time % 7 == 1) {
+                if(day == "Monday"){
+                  return("Tuesday");
+                }
+                else if(day == "Tuesday") {
+                  return("Wednesday");
+                }
+                else if(day == "Wednesday") {
+                  return("Thursday");
+                }
+                else if(day == "Thursday") {
+                  return("Friday");
+                }
+                else if(day == "Friday") {
+                  return("Saturday");
+                }
+                else if(day == "Saturday") {
+                  return("Sunday");
+                }
+                else if(day == "Sunday") {
+                  return("Monday");
+                }
+              }
+
+}
+
+// Tests
+console.log(whichDay("Thursday", 4) == "Monday");
+console.log(whichDay("Tuesday", 100) == "Thursday");
